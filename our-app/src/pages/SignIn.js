d5b3a15bae7590f1cf2dca38490cb93fc8265c50
ScriptKidHicks <Tammashicks@gmail.com>
2021-08-21 09:18:09 -0700

    pushing signin card content

--- our-app/src/pages/SignIn.js
+++ our-app/src/pages/SignIn.js
@@ -1,5 +1,22 @@
+import classes from "../cssModules/SignIn.module.css";
+
 function SignIn() {
-  return <div>Signin Page</div>;
+  return (
+    <div className={classes.main}>
+      <h2 className={classes.signInTitle}>Sign In</h2>
+      <form>
+        <label for="username">Username:</label>
+        <input type="text" name="username"></input>
+        <br></br>
+        <label for="pass">Password:</label>
+        <input type="password" name="pass"></input>
+        <br></br>
+        <button type="submit" formaction="">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
